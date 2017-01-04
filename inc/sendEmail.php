26b65d24eaef9f9a856a4478d70abc7a8fcf9a20
unknown <aassou.abdelilah@gmail.com>
2017-01-04 17:07:26 +0000

    Theme Update - Date : 03/01/2017

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'aassou.abdelilah@gmail.com';
+$siteOwnersEmail = 'user@website.com';
 
 
 if($_POST) {
@@ -11,12 +11,28 @@ if($_POST) {
    $subject = trim(stripslashes($_POST['contactSubject']));
    $contact_message = trim(stripslashes($_POST['contactMessage']));
 
+   // Check Name
+	if (strlen($name) < 2) {
+		$error['name'] = "Nom, SVP!";
+	}
+	// Check Email
+	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
+		$error['email'] = "Email, SVP!";
+	}
+	// Check Message
+	if (strlen($contact_message) < 15) {
+		$error['message'] = "Message, SVP!";
+	}
+   // Subject
+	if ($subject == '') { $subject = "Nodom Contact Form Submission"; }
+
+
    // Set Message
-    $message .= "Email from: " . $name . "<br />";
-    $message .= "Email address: " . $email . "<br />";
-    $message .= "Message: <br />";
-    $message .= $contact_message;
-    $message .= "<br /> ----- <br /> This email was sent from NodomTech contact form. <br />";
+   $message .= "Email from: " . $name . "<br />";
+	$message .= "Email address: " . $email . "<br />";
+   $message .= "Message: <br />";
+   $message .= $contact_message;
+   $message .= "<br /> ----- <br /> This email was sent from NodomTech contact form. <br />";
 
    // Set From: header
    $from =  $name . " <" . $email . ">";
@@ -27,10 +43,27 @@ if($_POST) {
  	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    mail($siteOwnersEmail, $subject, $message, $headers)
+
+   if (!$error) {
+
+      ini_set("sendmail_from", $siteOwnersEmail); // for windows server
+      $mail = mail($siteOwnersEmail, $subject, $message, $headers);
+
+		if ($mail) { echo "OK"; }
+      else { echo "Une erreur est survenue, SVP renvoyer votre email."; }
+		
+	} # end if - no validation error
+
+	else {
+
+		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
+		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
+		
+		echo $response;
+
+	} # end if - there was a validation error
 
 }
-echo 'test';
-//header('Location:../index.html');
 
 ?>
